04b698046d52eb47e2ae4123aeb79ee556e4a29b
Jack McCarthy <jackmccarthy@Jacks-MacBook-Air.local>
2024-05-29 15:25:45 -0700

    Adding set table to DB

--- index.ts
+++ index.ts
@@ -2,12 +2,17 @@ import extractCardData from "./src/extractCardData"
 import getCardsInSet from "./src/getCardsInSet"
 import getSets from "./src/getSets"
 import insertPokemonCardToDatabase from "./src/insertPokemonCardToDatabase"
+import insertPokemonSetToDatabase from "./src/insertPokemonSet"
 import { SupportedSeries } from "./types/Series"
 require('@dotenvx/dotenvx').config()
 const seriesList = <SupportedSeries[]>["Sun & Moon", "Sword & Shield", "Scarlet & Violet"]
 
 async function downloadCardInfo(seriesName: SupportedSeries) {
     const setsInSeries = await getSets(seriesName)
+    for (const cardSet of setsInSeries) {
+       await insertPokemonSetToDatabase(cardSet)
+
+    } 
     for (const cardSet of setsInSeries) {
         console.log(cardSet.name, cardSet.printedTotal, cardSet.total)
         const cardsInSet = await getCardsInSet(cardSet.id, cardSet.total)
---
+++ src/insertPokemonSet.ts
@@ -0,0 +1,26 @@
+import { PokemonTCG } from "pokemon-tcg-sdk-typescript";
+import { PokemonCards, PokemonSets } from "../database";
+import { PokemonCardType } from "../types/PokemondCard";
+
+
+export default async function insertPokemonSetToDatabase(pokemonSet:PokemonTCG.Set){
+    
+console.log(pokemonSet)
+    const cardExistsCount = await PokemonSets.count({
+        where: {
+            setid : pokemonSet.id
+        }
+    })
+    if(cardExistsCount === 0 ){
+       await  PokemonSets.create({
+        setid: pokemonSet.id,
+        setname:pokemonSet.name,
+        setlogourl:pokemonSet.images.logo,
+        setsymbolurl:pokemonSet.images.symbol,
+        setreleasedate:pokemonSet.releaseDate
+       })
+    }
+    else{
+        console.log("set exists:", pokemonSet.id)
+    }
+}
