53a8e0cae4f412492bbb17db3e1e2f5d5c5c1eb2
Jack McCarthy <jackmccarthy@Jacks-MacBook-Air.local>
2024-05-31 10:22:12 -0700

    Not logging this

--- src/getLatestTCGPlayerDate.ts
+++ src/getLatestTCGPlayerDate.ts
@@ -5,7 +5,8 @@ export default async function getLatestTCGPlayerPriceDate(){
     let maxPriceDate = await PokemonCards.findOne({
         limit: 1,
         attributes:["tcgplayerpricedate"],
-        order:[["tcgplayerpricedate", "desc"]]
+        order:[["tcgplayerpricedate", "desc"]],
+        logging:false
     })
     let result = <Date>maxPriceDate?.get("tcgplayerpricedate")
     return result.toISOString()
